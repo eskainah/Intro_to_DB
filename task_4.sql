f2c5015a18558cd6ee7eb3b962fc4e343c67610c
eskainah <eskainah@gmail.com>
2024-07-29 02:14:21 +0300

    print table description

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+-- Query the information_schema to get the table description
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_KEY AS 'Column Key',
+    EXTRA AS 'Extra Information'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
